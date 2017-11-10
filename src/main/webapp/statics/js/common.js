2ec5d361a9d6af9463c3a3380be75065306e6dc9
DESKTOP-LE3P71R\Yunfei <baiyunfeiii@foxmail.com>
2017-11-10 22:54:06 +0800

    update layui to 1.0.9

--- src/main/webapp/statics/js/common.js
+++ src/main/webapp/statics/js/common.js
@@ -79,3 +79,19 @@ function getSelectedRows() {
 function isBlank(value) {
     return !value || !/\S/.test(value)
 }
+
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
